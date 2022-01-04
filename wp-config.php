a448857330a617cf149e504dfe78792892044064
Keneth <kenneth.rosales15@gmail.com>
2022-01-04 10:53:30 -0600

    Update

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'webmedia_coalitiontest' );
+define( 'DB_NAME', 'webmedia_inpsyde' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
